64c3a2548e8b01f53bf6e56e7cf75a2bed301973
marco-bor <bortolanmarco@gmail.com>
2022-12-02 11:02:52 +0100

    Cleanup day1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,56 +1,24 @@
 fn main() {
     let input = include_str!("input.txt");
 
-    problem1(input);
-    problem2(input);
+    println!("Problem 1: {}", problem1(input));
+    println!("Problem 2: {}", problem2(input));
 }
 
-fn problem1(input: &str) {
-    let mut current_calories = 0u32;
-    let mut max_calories = 0u32;
-
-    input.lines().for_each(|s| {
-        if s.is_empty() {
-            max_calories = max_calories.max(current_calories);
-            current_calories = 0;
-        } else if let Ok(parsed) = s.parse::<u32>() {
-            current_calories += parsed;
-        } else {
-            println!("Could not parse line '{}', skipping", s)
-        }
-    });
-
-    // fix: file not ending with newline
-    if current_calories > 0 {
-        max_calories = max_calories.max(current_calories);
-    }
-
-    println!("Answer 1 is {}", max_calories)
+fn problem1(input: &str) -> u32 {
+    input
+        .split("\n\n")
+        .map(|group| group.lines().flat_map(|s| s.parse::<u32>()).sum())
+        .max()
+        .unwrap()
 }
 
-fn problem2(input: &str) {
-    let mut current_calories = 0u32;
-    let mut calories: Vec<u32> = vec![];
+fn problem2(input: &str) -> u32 {
+    let mut calories: Vec<u32> = input
+        .split("\n\n")
+        .map(|group| group.lines().flat_map(|s| s.parse::<u32>()).sum())
+        .collect();
 
-    input.lines().for_each(|s| {
-        if s.is_empty() {
-            calories.push(current_calories);
-            current_calories = 0;
-        } else if let Ok(parsed) = s.parse::<u32>() {
-            current_calories += parsed;
-        } else {
-            println!("Could not parse line '{}', skipping", s)
-        }
-    });
-
-    // fix: file not ending with newline
-    if current_calories > 0 {
-        calories.push(current_calories);
-    }
-
-    // sort ascending, then sum last 3 items
     calories.sort_unstable();
-    let top3: u32 = calories.iter().rev().take(3).sum();
-
-    println!("Answer 2 is {}", top3)
+    calories.iter().rev().take(3).sum()
 }
