dc64d934b6723e060b3734d991d422ce9135d613
marco-bor <bortolanmarco@gmail.com>
2022-12-03 10:23:19 +0100

    Day 3

---
+++ day03/src/main.rs
@@ -0,0 +1,54 @@
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("Problem 1: {}", problem1(input));
+    println!("Problem 2: {}", problem2(input));
+}
+
+const fn priority(char: char) -> u32 {
+    match char {
+        'a'..='z' => 1 + (char as u32) - ('a' as u32),
+        'A'..='Z' => 27 + (char as u32) - ('A' as u32),
+        _ => 0,
+    }
+}
+
+fn problem1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|l| l.split_at(l.len() / 2))
+        .flat_map(|x| x.0.chars().find(|c| x.1.contains(*c)))
+        .map(|c| priority(c))
+        .sum()
+}
+
+fn problem2(input: &str) -> u32 {
+    input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks_exact(3)
+        .flat_map(|g| find_badge(g))
+        .map(|badge| priority(badge))
+        .sum()
+}
+
+fn find_badge(group: &[&str]) -> Option<char> {
+    if let [first, second, third] = group {
+        first.find(|c| second.contains(c) && third.contains(c)).map(|i| first.as_bytes()[i] as char)
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_priorities() {
+        assert_eq!(priority('a'), 1);
+        assert_eq!(priority('z'), 26);
+        assert_eq!(priority('A'), 27);
+        assert_eq!(priority('Z'), 52);
+    }
+}
