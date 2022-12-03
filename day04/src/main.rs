12e08b0aa10c9f304b7ca6e1c3778a79a26481bb
marco-bor <bortolanmarco@gmail.com>
2022-12-03 10:24:12 +0100

    Prepare day 4

---
+++ day04/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("testdata.txt");
+
+    println!("Problem 1: {}", problem1(input));
+    println!("Problem 2: {}", problem2(input));
+}
+
+fn problem1(input: &str) -> u32 {
+    0
+}
+
+fn problem2(input: &str) -> u32 {
+    0
+}
