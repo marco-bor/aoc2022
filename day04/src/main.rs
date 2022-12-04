346f7cd68e1ea366ccfd80d2e9b9ab7b13ca759d
marco-bor <bortolanmarco@gmail.com>
2022-12-04 07:01:34 +0100

    Day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,14 +1,66 @@
+use std::str::FromStr;
+
 fn main() {
-    let input = include_str!("testdata.txt");
+    let input = include_str!("input.txt");
 
     println!("Problem 1: {}", problem1(input));
     println!("Problem 2: {}", problem2(input));
 }
 
 fn problem1(input: &str) -> u32 {
-    0
+    input
+        .lines()
+        .flat_map(|l| l.split_once(','))
+        .flat_map(|r| {
+            let (Ok(a),Ok(b)) = (r.0.parse::<Range>(), r.1.parse::<Range>()) else {
+                return None
+            };
+            Some((a, b))
+        })
+        .filter(|(a, b)| a.contains(&b) || b.contains(&a))
+        .count() as u32
 }
 
 fn problem2(input: &str) -> u32 {
-    0
+    input
+        .lines()
+        .flat_map(|l| l.split_once(','))
+        .flat_map(|r| {
+            let (Ok(a),Ok(b)) = (r.0.parse::<Range>(), r.1.parse::<Range>()) else {
+                return None
+            };
+            Some((a, b))
+        })
+        .filter(|(a, b)| a.overlaps(&b))
+        .count() as u32
+}
+
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl Range {
+    fn contains(&self, other: &Range) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &Range) -> bool {
+        !(self.start > other.end || self.end < other.start)
+    }
+}
+impl FromStr for Range {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let Some((start, end)) = s.split_once('-') else {
+            return Err(());
+        };
+
+        let (Ok(start), Ok(end)) = (start.parse::<u32>(), end.parse::<u32>()) else {
+            return Err(());
+        };
+
+        Ok(Self { start, end })
+    }
 }
